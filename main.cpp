145ba039208b19f7e1491b63e8388c8585f83bc9
Alice(CheremnykhYA) <yurchest@gmail.com>
2020-03-19 00:53:04 +0300

    Вывод разности двух чисел

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@ int main()
     int a, b;
     cin >> a >> b;
 
-    cout << "A + B = " << a + b << '\n';
+    cout << "A + B = " << a + b << '\n'
+         << "A - B = " << a - b << '\n';
 
 
     return 0;

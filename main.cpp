a3ef46227c05afab0a83f538e9c19c4cbcde65f6
Alice(CheremnykhYA) <yurchest@gmail.com>
2020-03-19 01:36:00 +0300

    Вывод деления двух чисел

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ int main()
 
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
-         << "A * B = " << a * b << '\n';
+         << "A * B = " << a * b << '\n'
+         << "A / B = " << a / b << '\n';
 
 
     return 0;

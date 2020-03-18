89e813a7713e3ead6afae0ebe883f9c45a188461
Alice(CheremnykhYA) <yurchest@gmail.com>
2020-03-19 01:43:17 +0300

    MAX

--- main.cpp
+++ main.cpp
@@ -13,6 +13,11 @@ int main()
          << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n';
 
+      double max;
+        if (a > b) max = a;
+        if (b > a) max = b;
+        if (b == a) cout << "A = B";
+            else cout << "MAX = " << max;
 
     return 0;
 }
